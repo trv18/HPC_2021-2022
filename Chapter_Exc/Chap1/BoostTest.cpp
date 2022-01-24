e7d542dec115396f8859d5403a52c45b125b1d2c
vdwti <vdwtim.99@gmail.com>
2022-01-24 08:29:17 +0000

    Added Correct Header path

--- Chapter_Exc/Chap1/BoostTest.cpp
+++ Chapter_Exc/Chap1/BoostTest.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <home/vdwti/HPC_21_22/Boost/program_options.hpp>
+#include "boost/program_options.hpp"
 namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
